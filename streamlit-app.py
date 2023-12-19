15dec78e74abb16bf9a9135c719bb0c0c899224e
Gouttard <achille.gouttard@kuehne-nagel.com>
2023-12-19 14:47:09 +0100

    possibility to filter according to the date. Now working on charts

--- streamlit-app.py
+++ streamlit-app.py
@@ -6,21 +6,28 @@ import geopandas as gpd
 from folium.plugins import MarkerCluster
 import sqlite3
 import datetime
-import traceback
 from jinja2 import Template
+import traceback
+
+semesters = {"first": datetime.datetime(1999, 8, 31),
+            "second": datetime.datetime(1999, 2, 28)}
 
 categ = ["DPT-Agence", "Projet-Commerce-Agence", "Projet-DPP-Agence",
-            "Projet Contrôle de gestion", "Projet LTL-FTL", "Projet-DRC-Agence",
-                "Projet-Ops-Agence"]
+        "Projet Contrôle de gestion", "Projet LTL-FTL", "Projet-DRC-Agence",
+        "Projet-Ops-Agence"]
 
 name_table = {'Catégorie':"categorie", "Titre":"titre_projet","Description":"descriptif", "Début": "date_debut","Fin":"date_fin", "Agence":"code_agence"}
 
-if st.session_state.get('step') is None:
-    st.session_state['step'] = 0
-
-if "visibility" not in st.session_state:
-    st.session_state.visibility = "visible"
-    st.session_state.disabled = False
+def  init_session_state():
+    if st.session_state.get('step') is None:
+        st.session_state['step'] = 0
+    
+    if st.session_state.get('semestre') is None:
+        st.session_state["semestre"] = "all"
+        
+    if "visibility" not in st.session_state:
+        st.session_state.visibility = "visible"
+        st.session_state.disabled = False
 
 def edit_db(edited, cursor, conn, index):
     for i in edited:
@@ -35,7 +42,7 @@ def edit_db(edited, cursor, conn, index):
             sql_template = Template("""
             UPDATE Projets
                 SET {% for key, value in data.items() %}
-                    {{ key }} = {{ '?' }}{% if not loop.last %},{% endif %}
+                    {{ key }} = {{ '?' }} {% if not loop.last %},{% endif %}
                     {% endfor %}
             WHERE projet_id = {{ '?' }}
             """)
@@ -58,8 +65,7 @@ def delete_rows(to_delete, cursor, conn):
     conn.commit()
 
 def callback(index, cursor, conn, obj, agence):
-    if "my_key" in st.session_state:
-        
+    if "my_key" in st.session_state:        
         edited = st.session_state.my_key['edited_rows']
         if len(edited) > 0:
             edit_db(edited, cursor, conn, index)
@@ -68,7 +74,7 @@ def callback(index, cursor, conn, obj, agence):
         if len(deleted) > 0:
             deleted = [int(index.iloc[i]) for i in deleted]
             delete_rows(deleted, cursor, conn)
-            
+
         added = st.session_state.my_key["added_rows"]
         if len(added) > 0:
             x = st.session_state.my_key["added_rows"].pop()
@@ -78,52 +84,51 @@ def callback(index, cursor, conn, obj, agence):
                 obj.submit(added_row=x)
         if len(edited) > 0 or len(deleted) > 0 or len(added) > 0:
             st.cache_data.clear()
-        if len(added) > 0:        
+        if len(added) > 0:
             st.session_state["my_key"]["added_rows"].clear()
  
 def retrieve_information(rows, name, count):
-    string = f"Agence {name}<br>"
+    string = f"<b>Agence {name}</b><br>"
     string += f"Nombre de projets: {count} <br>"
     for row in rows:
         string += f"<br>{row[1]}: {row[2]}, {row[3]} <br>"
     return string
 
 @st.cache_data()
-def request_db(_agences, _conn):
+def request_db(_agences, _conn, _condition):
     cursor = _conn.cursor()
     count = []
     dic_infos = {}
-
     for i in _agences:
         # Execute the SQL query to fetch agency information
-        query = f"SELECT COUNT(*) FROM Projets WHERE code_agence = '{i}'"
+        query = f"SELECT COUNT(*) FROM Projets WHERE code_agence = '{i}'" + _condition
         cursor.execute(query)
         project_count = cursor.fetchone()
-
         # Fetch the result
         if project_count is not None:
             try:
                 project_count = project_count[0]
-                # Print the result
-                print(f"{i} nbr de projets: {project_count}")
+            # Print the result
             except Exception as e:
                 print(e)
                 project_count = 0
         else:
             project_count = 0
         count.append(project_count)
-        sec_query = f"SELECT * FROM Projets WHERE code_agence = '{i}'"
+        sec_query = f"SELECT * FROM Projets WHERE code_agence = '{i}'" +  _condition
         cursor.execute(sec_query)
         rows = cursor.fetchall()
         dic_infos[i] = retrieve_information(rows,i, project_count)
     cursor.close()
     return count, dic_infos
 
-
 class App():
-    def __init__(self, geo_data, conn, map_agencies, cursor, localisation):
+    def __init__(self, geo_data, conn, map_agencies, cursor, localisation, year):
         self.geo_data = geo_data
+        self.year = year
         self.conn = conn
+        self.get_date()
+        self.today = datetime.date.today()
         self.localisation = localisation
         self.markers = {}
         self.cursor = cursor
@@ -132,22 +137,22 @@ class App():
         self.draw_map()
 
     def submit(self, selected_categ="", title="", description="", date_debut="", date_fin="", selected_agence="", added_row=""):
-                if added_row != "":
-                    new_mapping = {}
-                    try:
-                        for key, value in added_row.items():
-                            new_mapping[name_table[key]] = value
-                    except:
-                        return
-                    added_row = new_mapping
-                    return
-                query = "INSERT INTO Projets (categorie, titre_projet, descriptif, date_debut, date_fin, code_agence) VALUES (?, ?, ?, ?, ?, ?)"
-                parameters = (selected_categ, title, description, date_debut, date_fin, selected_agence)
-                self.cursor.execute(query, parameters)
-                # Commit the changes to the database
-                self.conn.commit()
-                st.write(f"Tâche attribuée à l'agence: {selected_agence}")      
-                st.cache_data.clear()
+        if added_row != "":
+            new_mapping = {}
+            try:
+                for key, value in added_row.items():
+                    new_mapping[name_table[key]] = value
+            except:
+                return
+            added_row = new_mapping
+            return
+        query = "INSERT INTO Projets (categorie, titre_projet, descriptif, date_debut, date_fin, code_agence) VALUES (?, ?, ?, ?, ?, ?)"
+        parameters = (selected_categ, title, description, date_debut, date_fin, selected_agence)
+        self.cursor.execute(query, parameters)
+        # Commit the changes to the database
+        self.conn.commit()
+        st.write(f"Tâche attribuée à l'agence: {selected_agence}")      
+        st.cache_data.clear()
 
     def add_agence(self, agence="", category="", title="", description="", begin="", end=""):
         if agence not in self.agences_name:
@@ -159,15 +164,43 @@ class App():
         if begin >= end:
             st.write(":red[Merci de sélectionner une date de fin ultérieure à la date de début.]")
             return
+        begin = datetime.datetime.combine(begin, datetime.datetime.min.time())
+        end = datetime.datetime.combine(end, datetime.datetime.min.time())
+
         self.submit(category, title, description, begin, end, agence)
         st.write(f":green[{title} ajoutée à l'agence {agence}]")
         st.rerun()
 
+    def get_date(self):
+        if st.session_state["semestre"] == "all":
+            self.condition = ""
+        if st.session_state["semestre"] == semesters["first"]:
+            self.condition = "AND (strftime('%m', date_debut) < '03' OR strftime('%m', date_debut) > '08')"
+        if st.session_state["semestre"] == semesters["second"]:
+            self.condition = "AND (strftime('%m', date_debut) > '02' AND strftime('%m', date_debut) < '09')"
+
+    def make_request(self, agence):
+        time = 'where ' + self.condition[4:] if len(self.condition) > 1 else ""
+        if agence == "Toutes les agences" and self.time_interval == False:
+            query = f"SELECT * FROM Projets {time}"
+            rows = self.cursor.execute(query).fetchall()
+
+        elif agence == "Toutes les agences" and self.time_interval == True:
+            query = f"SELECT * FROM Projets where date_debut >= ? and date_fin <= ?"
+            rows = self.cursor.execute(query, (self.debut, self.fin)).fetchall()
+
+        elif agence != "Toutes les agences" and self.time_interval == False:
+            query = f"SELECT * FROM Projets where code_agence = ? " + self.condition
+            rows = self.cursor.execute(query, (str(agence), )).fetchall()
+
+        else:
+            query = f"SELECT * FROM Projets where code_agence = ? and  date_debut >= ? and date_fin <= ?"
+            rows = self.cursor.execute(query, (str(agence), self.debut, self.fin)).fetchall()
+        return rows
+    
     def display_editable_df(self, agence):
-        query = f"SELECT * FROM Projets where code_agence = ?" if agence != "Toutes les agences" else "SELECT * FROM Projets"
+        rows = self.make_request(agence)
         try:
-            rows = self.cursor.execute(query, (str(agence), )).fetchall() \
-                if agence != "Toutes les agences" else self.cursor.execute(query).fetchall()
             df = pd.DataFrame(rows, columns=['Index', 'Catégorie', 'Titre', 'Description', 'Début', 'Fin', "Agence"])
             cols = df.columns.tolist()
             cols = [cols[-1]] + cols[:-1]
@@ -183,7 +216,7 @@ class App():
         # Set the layout to a menu-like structure
         sidebar = st.sidebar
 
-        # Create a button to display the form
+        # Create a form to add an agency
         with sidebar:
             agences = [i for i in self.agences_name]
             agences = list(sorted(agences))
@@ -209,24 +242,23 @@ class App():
                     disabled=st.session_state.disabled,
                     placeholder="Description",
                 )
-                today = datetime.date.today()
-                first_date = st.date_input("Date de début", today )
-                second_date = st.date_input("Date de fin", today)
+                self.today = datetime.date.today()
+                first_date = st.date_input("Date de début", self.today )
+                second_date = st.date_input("Date de fin", self.today)
 
                 submitted = st.form_submit_button("Submit")
                 if submitted:
+                    print("Semestre: ", st.session_state["semestre"])
                     self.add_agence(agence=agence_picker, category=category, title=title,
-                               description=description, begin=first_date, end=second_date)
-
+                                description=description, begin=first_date, end=second_date)
 
     def mark_card(self):
             colors = ["green", "yellow", "red"]
-
             longitudes = self.localisation["longitude"].to_list()
             latitudes = self.localisation["latitude"].to_list()
             agences = self.localisation["code_agence"].to_list()
             self.agences_name = agences
-            counts, infos = request_db(agences, self.conn)
+            counts, infos = request_db(agences, self.conn, self.condition)
             marker_cluster = MarkerCluster().add_to(self.map_agencies)
             for i in range(len((longitudes))):
                 agence = agences[i]
@@ -241,8 +273,7 @@ class App():
                                         text_color="black",
                                         number=agence,
                                         inner_icon_style="margin-top:0;",
-                                        border_width=4
-                                    )
+                                        border_width=4)
                 marker = folium.Marker(location=[y, x],
                                     icon=icon,
                                     id=f'marker-{i}',
@@ -252,7 +283,47 @@ class App():
                 marker.add_to(marker_cluster)
             self.marker_cluster = marker_cluster            
 
+    def select_periods(self, debut, fin):
+        self.time_interval = True
+        self.debut = datetime.datetime.combine(debut, datetime.datetime.min.time())
+        self.fin = datetime.datetime.combine(fin, datetime.datetime.min.time())
+        return
+
+    def display_time(self, col1, col2, col3):
+        with col1:
+                if st.button("Semestre 1"):
+                    st.session_state["semestre"] = semesters["first"]
+                    self.time_interval == False
+                    print(st.session_state["semestre"])
+                    st.cache_data.clear()                                                 
+                    st.rerun()                                                 
+
+        with col2:
+            if st.button("Semestre 2"):
+                self.time_interval == False
+                st.session_state["semestre"] = semesters["second"] 
+                st.cache_data.clear()
+                st.rerun()                                                 
+        with col3:
+            if st.button("Année"):
+                self.time_interval == False
+                st.session_state["semestre"] = "all"
+                st.cache_data.clear()                                                 
+        
     def draw_map(self):
+        self.time_interval = False
+        col_1, col_2, col_3 = st.columns(3)
+        with col_1:
+            debut = st.date_input("Début", self.today )
+        with col_2:
+            fin = st.date_input("Fin", self.today)
+        with col_3:
+            st.markdown("<br>", unsafe_allow_html=True)
+            selectionner = st.button("Sélectionner")
+        if selectionner:
+            self.select_periods(debut, fin)
+        col1, col2, col3 = st.columns(3)
+        self.display_time(col1, col2, col3)
         if len(self.markers) == 0:
             self.mark_card()
         # Display the map in Streamlit
@@ -269,7 +340,13 @@ def fetch_and_clean_data(_conn):
     geo_data = gpd.read_file("departements.geojson")
     query = f"SELECT * FROM Agences"
     localisation = pd.read_sql_query(query, _conn)
-    return geo_data, localisation
+
+    today = datetime.date.today()
+    if today.month > semesters["first"].month:
+        year = today.year
+    else:
+        year = today.year - 1
+    return geo_data, localisation, year
 
 
 @st.cache_resource(hash_funcs={folium.Map: lambda _: None})
@@ -280,13 +357,12 @@ def create_map(_geo_data):
     return map
 
 def main():
-
+    init_session_state()
     with sqlite3.connect('planning.db', check_same_thread=False) as conn:
         cursor = conn.cursor()
-        # Register the close_connection function to be called at exit
-        geo_data, localisation = fetch_and_clean_data(conn)
+        geo_data, localisation, year = fetch_and_clean_data(conn)
         map_agency = create_map(geo_data)
-        app = App(geo_data, conn, map_agency, cursor, localisation)
+        app = App(geo_data, conn, map_agency, cursor, localisation, year)
 
 
 if __name__=="__main__":
